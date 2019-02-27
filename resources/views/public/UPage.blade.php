456b9406bec3edc75829adbd18373a1658e29294
unknown <sebastian.villadam@autonoma.edu.co>
2019-02-27 11:50:32 -0500

    finalizacion estilo pagina universidad

--- resources/views/public/UPage.blade.php
+++ resources/views/public/UPage.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.public_structure')
+@section('links')
+<link rel="stylesheet" href="/css/UPage_Style.css">
+@stop
 @section('content')
 <!--U title-->
-<div class="container-fluid padding">
+<div class="container-fluid padding" id="title">
   <div class="text-center">
     <h1>Universidad de manizales</h1>
+    <hr>
   </div>
 </div>
 
 <!--parking lots section-->
-<div class="container-fluid padding">
+<div class="container-fluid padding center" id="table">
   <div class="list-group">
     <a href="#" class="list-group-item list-group-item-action">Parqueadero 1</a>
     <a href="#" class="list-group-item list-group-item-action">Parqueadero 2</a>
