86013f28e85958c7d1fc7a31f030e2300d452750
sebastian.Villadam <sebastian.villadam@autonoma.edu.co>
2019-04-24 12:13:40 -0500

    Se aplico estilo a la pagina de erro 403

--- resources/views/public/errorWindow.blade.php
+++ resources/views/public/errorWindow.blade.php
@@ -2,9 +2,16 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/error_style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <title></title>
   </head>
   <body>
-    <h1>NO TIENE LOS PERMISOS REQUERIDOS PARA ENTRAR A ESTA DIRECCION</h1>
+    <div class="container text-center">
+      <h1>ERROR 403</h1>
+      <h2>No tienes permiso de entrar a la ruta que desas</h2>
+      <button type="button" class="btn btn-outline-secondary" id="btn-card" onclick="location.href='/home'">Regresar</button>
+    </div>
   </body>
 </html>
